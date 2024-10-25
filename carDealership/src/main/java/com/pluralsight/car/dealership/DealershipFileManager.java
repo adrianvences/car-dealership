27cef9ce75f7e827248b42e288fc543cb1e68fcc
adrianvences9820 <adrianvences9820@gmail.com>
2024-10-24 21:52:20 -0400

    started phase 2 on car dealership workshop

--- carDealership/src/main/java/com/pluralsight/car/dealership/DealershipFileManager.java
+++ carDealership/src/main/java/com/pluralsight/car/dealership/DealershipFileManager.java
@@ -1,4 +1,22 @@
 package com.pluralsight.car.dealership;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class DealershipFileManager {
+    static Dealership getDealership(){
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/Data/inventory.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void saveDealership(Dealership dealerShip){
+
+    }
+
 }
